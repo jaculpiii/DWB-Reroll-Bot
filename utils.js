4ce6f49200350b1b5dde6ba9f9eea68682bdb2dc
Glitch (like-wheat-starflower) <none>
2022-10-24 22:26:10 +0000

    🍐🔤 Updated with Glitch

--- utils.js
+++ utils.js
@@ -40,7 +40,7 @@ export async function DiscordRequest(endpoint, options) {
 
 // Simple method that returns a random emoji from list
 export function getRandomEmoji() {
-  const emojiList = ['??','??','??','??','??','??','??','??????','??','??','??','??','??','?','??'];
+  const emojiList = ['😭','😄','😌','🤓','😎','😤','🤖','😶‍🌫️','🌏','📸','💿','👋','🌊','✨','🪙'];
   return emojiList[Math.floor(Math.random() * emojiList.length)];
 }
 
