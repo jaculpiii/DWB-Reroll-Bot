585c1eddfade338a1d51d98f13d5f8dace399289
James C (aka Chuz) <jaculpiii@users.noreply.github.com>
2022-10-24 18:30:47 -0400

    Updates

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// Created for DWB RPG
+// (c) James "Chuz" Culp 2022
+
 import {
   EmbedBuilder,
 } from "discord.js";
@@ -60,8 +63,8 @@ app.post("/interactions", async function (req, res) {
   if (type === InteractionType.APPLICATION_COMMAND) {
     const { name } = data;
 
-    
-    
+
+
     // "reroll" or "rr" guild commands
     if (name === "reroll" || name === "rr") {
       const userId = req.body.member.user.id;
@@ -97,8 +100,8 @@ app.post("/interactions", async function (req, res) {
       }
     }
 
-    
-    
+
+
     // "add" guild command
     if (name === "add") {
       const userId = req.body.member.user.id;
@@ -157,8 +160,8 @@ app.post("/interactions", async function (req, res) {
       });
     }
 
-    
-    
+
+
     // "bal" guild command
     if (name === "bal") {
       const userId = req.body.member.user.id;
@@ -186,7 +189,7 @@ app.post("/interactions", async function (req, res) {
     }
 
 
-    
+
     // "reset" guild command
     if (name === "reset") {
       // If they selected "Yes" and they are Chuz ('1033603369887092786') then we wipe the db
@@ -203,36 +206,43 @@ app.post("/interactions", async function (req, res) {
         return res.send({
           type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
           data: {
-            // Fetches a random emoji to send from a helper function
             content: "You are not authorized to use this command! Only DWB can reset the database.",
           },
         });
       }
     }
-    
-    
+
+
+
     // "report" guild command
     if (name === "report") {
-      let users = [];
+      // Only allow DWB or Chuz use this command
+      if ( req.body.data.id !== "1033603369887092786" && req.body.data.id !== "182658928323198976" ) {
+        await User.destroy({ truncate: true });
+        return res.send({
+          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+          data: {
+            content: "You are not authorized to use this command! Only DWB can run the report.",
+          },
+        });
+      }
 
+      let users = [];
       if( req.body.data.options && req.body.data.options[0].value ) {
         users.push(await User.findOne({ where: { id: req.body.data.options[0].value } }));
       } else {
         users = await User.findAll({ raw: true });
       }
-console.log(users);
-      
+
       let content = "**Reroll Token Report\nMember\n\t\tRemaining\t\tPurchased**\n";
       users.forEach((user) => {
         let uname = util.format("<@%s>", user.id);
-//        uname = uname.padEnd(30, ' ');
         let currency = util.format("%i", user.currency);
         currency = currency.padEnd(3, ' ');
-console.log(uname);
+
         content += util.format("%s\n\t\t\t\t%s\t\t\t\t\t%s\n", uname, currency, user.total);
-//        content += "<@" + user.id + ">" + "     " + user.currency + "     " + user.total + "\n";
       })
-    
+
 
       return res.send({
         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
@@ -241,9 +251,9 @@ console.log(uname);
         }
       })
     }
-    
-    
-    
+
+
+
     // "help" guild command
     if( name === "help") {
       const exampleEmbed = {
@@ -273,49 +283,14 @@ console.log(uname);
         },
       };
 
-/*
-      const newEmbed = new Discord.MessageEmbed()
-      .setColor('#23dd17')
-      .setTitle('So you need help traveler?')
-      .setImage('https://static.wikia.nocookie.net/criticalrole/images/1/10/BlackSalander_-_The_Traveler.jpg/revision/latest?cb=20180824205702')
-      //This image was sourced from https://twitter.com/BlackSalander/status/1031952265593217024, I do not own this content, all rights reserved.
-      .setDescription('Glad you came to the right place, let me guide you!')
-      .addFields(
-          {name: 'Reroll', value: 'Syntax: `-buy reroll,rr` This command is used to reroll your epic.'},
-          {name: 'Add', value: 'Syntax: `-add [number] @user` This command is for admins and staff to add rerolls to your account'},
-          {name: 'Bal', value: 'Syntax: `-bal (@user)` This command will show you how many rerolls you currently have.'},
-          {name: 'Ping', value: 'Syntax: `-ping` This command is to test the bot and make sure it is online.'},
-          {name: 'Reset', value: 'Syntax: `-reset` This command is to delete all tokens and prevents anyone from using `-buy rr`.'}
-      );
-*/
-
-/*
-      const newEmbed = new EmbedBuilder()
-      .setColor(0x0099FF)
-      .setTitle('Some title')
-      .setURL('https://discord.js.org/')
-      .setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
-      .setDescription('Some description here')
-      .setThumbnail('https://i.imgur.com/AfFp7pu.png')
-      .addFields(
-        { name: 'Regular field title', value: 'Some value here' },
-        { name: '\u200B', value: '\u200B' },
-        { name: 'Inline field title', value: 'Some value here', inline: true },
-        { name: 'Inline field title', value: 'Some value here', inline: true },
-      )
-      .addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
-      .setImage('https://i.imgur.com/AfFp7pu.png')
-      .setTimestamp()
-      .setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
-*/      
-
-
       return res.send({
         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
         data: {
           embeds: [exampleEmbed],
         }
-      })     
+      })
+
+
 
     }
   }
@@ -336,3 +311,5 @@ app.listen(PORT, () => {
     HELP_COMMAND,
   ]);
 });
+
+
--- utils.js
+++ utils.js
@@ -40,10 +40,11 @@ export async function DiscordRequest(endpoint, options) {
 
 // Simple method that returns a random emoji from list
 export function getRandomEmoji() {
-  const emojiList = ['😭','😄','😌','🤓','😎','😤','🤖','😶‍🌫️','🌏','📸','💿','👋','🌊','✨','🪙'];
+  const emojiList = ['??','??','??','??','??','??','??','??????','??','??','??','??','??','?','??'];
   return emojiList[Math.floor(Math.random() * emojiList.length)];
 }
 
 export function capitalize(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
